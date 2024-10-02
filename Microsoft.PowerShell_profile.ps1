9380ac4cbaea54614e202b7839dfbd56abae1e0b
JulianDezitter <125573258+JulianDezitter@users.noreply.github.com>
2024-10-02 13:12:26 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -90,7 +90,8 @@ Update-PowerShell
 # Admin Check and Prompt Customization
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 function prompt {
-    if ($isAdmin) { "[" + (Get-Location) + "] # " } else { "[" + (Get-Location) + "] $ " }
+	$currentTime = Get-Date -Format "HH:mm:ss dd/MM/yyyy"
+    if ($isAdmin) { "[" + (Get-Location) + "] $currentTime # " } else { "[" + (Get-Location) + "] $currentTime $ " }
 }
 $adminSuffix = if ($isAdmin) { " [ADMIN]" } else { "" }
 $Host.UI.RawUI.WindowTitle = "PowerShell {0}$adminSuffix" -f $PSVersionTable.PSVersion.ToString()
